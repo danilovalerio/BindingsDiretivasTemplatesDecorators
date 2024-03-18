20d7020d011b7c1e0a5d7fbeebf37c3a9841f8db
danilovalerio <danilo_vs@hotmail.com>
2024-03-18 17:10:39 -0300

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'BindingsDiretivasTemplatesDecorators';
+  inputText = 'Texto Inicial';
+
+  //define a propriedade do type do nosso inpu
+  inputType = 'text';
+
+  isDisable = true;
 }
