4a253ece90a2494ba07ee49cb0b863b37f9e9f67
danilovalerio <danilo_vs@hotmail.com>
2024-03-19 14:50:26 -0300

    CSS CLASS BINDING

--- src/app/attribute-binding-css-class/attribute-binding-css-class.component.ts
+++ src/app/attribute-binding-css-class/attribute-binding-css-class.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-attribute-binding-css-class',
   templateUrl: './attribute-binding-css-class.component.html',
-  styleUrl: './attribute-binding-css-class.component.scss'
+  styleUrl: './attribute-binding-css-class.component.scss',
 })
-export class AttributeBindingCssClassComponent {
+export class AttributeBindingCssClassComponent implements OnInit {
+  ngOnInit(): void {}
 
+  isRedButton = true;
+  isGreen = true;
+  isRed = true;
+
+  isRedProp = true;
 }
